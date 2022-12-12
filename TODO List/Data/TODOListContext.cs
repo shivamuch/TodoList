3c32fbd2182e42cc7632825c0abc779eb195ad91
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2022-12-12 12:33:37 -0500

    Added data files and object files to the repository

---
+++ TODO List/Data/TODOListContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TODO_List.Models;
+
+namespace TODO_List.Data
+{
+    public class TODOListContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public TODOListContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            var connectionString = Configuration.GetConnectionString("TODOListContext");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+
+
+        //public TODOListContext (DbContextOptions<TODOListContext> options)
+        //    : base(options)
+        //{
+        //}
+
+        public DbSet<TODO_List.Models.TODOList> TODOList { get; set; } = default!;
+    }
+}
