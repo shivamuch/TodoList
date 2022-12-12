f41d42ed23db70a6e5aba6e53392c3bb8c1fd380
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2022-12-12 12:42:12 -0500

    resolved the file issue and added the rest program files

---
+++ TODO List/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TODO_List.Data;
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddDbContext<TODOListContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("TODOListContext") ?? throw new InvalidOperationException("Connection string 'TODOListContext' not found.")));
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+builder.Services.AddDbContext<TODOListContext>(options =>
+{
+    var ConnectionString = builder.Configuration.GetConnectionString("TODOListContext");
+    options.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+});
+
+
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=TODOList}/{action=Index}/{id?}");
+
+app.Run();
